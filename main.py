4053b121553ca648252c6ccc503aaefb44c44b3b
proxytype <shlomiruder@gmail.com>
2020-06-08 00:40:41 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+import argparse
 from ReverseProxy import ReverseProxy
 
-proxy = ReverseProxy("192.168.1.5", 8080, "www.mako.co.il", 443)
-proxy.start_proxy()
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-pa', action='store', dest='proxy_address',
+                    help='proxy address')
+
+parser.add_argument('-pp', action='store', dest='proxy_port', type=int,
+                    help='proxy port')
+
+parser.add_argument('-pda', action='store', dest='proxy_destination_address',
+                    help='proxy destination address')
+
+parser.add_argument('-pdp', action='store', dest='proxy_destination_port', type=int,
+                    help='proxy destination port')
+
+
+results = parser.parse_args()
+
+if results.proxy_address and results.proxy_port and results.proxy_destination_address and results.proxy_destination_port:
+
+    proxy = ReverseProxy(results.proxy_address, results.proxy_port, results.proxy_destination_address,
+                     results.proxy_destination_port)
+
+    proxy.start_proxy()
